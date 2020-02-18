8e939ac208b697bfa041d52bf02f7af3faa8a2fa
Niko Granö <niko@ironlions.fi>
2020-02-18 20:57:53 +0200

    Setup basic WS.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ *
+ * GameTactic Websocket 2020 — NOTICE OF LICENSE
+ * This source file is released under GPLv3 license by copyright holders.
+ * Please see LICENSE file for more specific licensing terms.
+ * @copyright 2019-2020 (c) GameTactic
+ * @author Niko Granö <niko@granö.fi>
+ *
+ */
+
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ *
+ * GameTactic Websocket 2020 — NOTICE OF LICENSE
+ * This source file is released under GPLv3 license by copyright holders.
+ * Please see LICENSE file for more specific licensing terms.
+ * @copyright 2019-2020 (c) GameTactic
+ * @author Niko Granö <niko@granö.fi>
+ *
+ */
+
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class      => ['all' => true],
+    Gos\Bundle\PubSubRouterBundle\GosPubSubRouterBundle::class => ['all' => true],
+    Gos\Bundle\WebSocketBundle\GosWebSocketBundle::class       => ['all' => true],
 ];
